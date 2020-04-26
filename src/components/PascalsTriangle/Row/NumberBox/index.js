d2c259c89768e3c79bcb9aadc0d01e189d824d5f
unknown <ashishuiux@gmail.com>
2020-04-26 15:00:02 -0700

    change Numberbox to functional component

--- src/components/PascalsTriangle/Row/NumberBox/index.js
+++ src/components/PascalsTriangle/Row/NumberBox/index.js
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
 import "./style.css";
 
-class NumberBox extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div className="number-box">{this.props.number}</div>;
-  }
-}
+let NumberBox = (props) => {
+  return <div className="number-box">{props.number}</div>;
+};
 
 export default NumberBox;
