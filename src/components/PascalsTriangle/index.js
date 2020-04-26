ec22db5cd8b084b975322a429350d7010f287c20
unknown <ashishuiux@gmail.com>
2020-04-26 15:06:13 -0700

    make Pascal's Triangle a functional component

--- src/components/PascalsTriangle/index.js
+++ src/components/PascalsTriangle/index.js
@@ -1,21 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Row from "./Row";
 import "./style.css";
 
-class PascalsTriangle extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="pascals-triangle">
-        {this.props.triangle.map((x, i) => (
-          <Row numberOfBoxes={i + 1} key={i} elements={x} />
-        ))}
-      </div>
-    );
-  }
-}
+let PascalsTriangle = (props) => {
+  return (
+    <div className="pascals-triangle">
+      {props.triangle.map((x, i) => (
+        <Row numberOfBoxes={i + 1} key={i} elements={x} />
+      ))}
+    </div>
+  );
+};
 
 export default PascalsTriangle;
