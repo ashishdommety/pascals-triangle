678903c844d9bce7f61648b6f358ccdec3c0b33c
unknown <ashishuiux@gmail.com>
2020-04-26 15:03:58 -0700

    make Number box into a functional component

--- src/components/PascalsTriangle/Row/index.js
+++ src/components/PascalsTriangle/Row/index.js
@@ -1,22 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import NumberBox from "./NumberBox";
 import "./style.css";
 
-class Row extends Component {
-  constructor(props) {
-    super(props);
-    this.createBoxesArray = this.createBoxesArray.bind(this);
-  }
-  createBoxesArray = () => {
-    let boxes = this.props.elements.map((elem, i) => (
+let Row = (props) => {
+  let createBoxesArray = () => {
+    let boxes = props.elements.map((elem, i) => (
       <NumberBox number={elem} key={i} />
     ));
     return <div className="row">{boxes}</div>;
   };
 
-  render() {
-    return this.createBoxesArray();
-  }
-}
+  return createBoxesArray();
+};
 
 export default Row;
