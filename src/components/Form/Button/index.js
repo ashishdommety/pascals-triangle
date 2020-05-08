5a73d2fcab307c5ffa9de4c676a5332cb0c5d9f4
unknown <ashishuiux@gmail.com>
2020-05-08 12:29:22 -0700

    style buttons

--- src/components/Form/Button/index.js
+++ src/components/Form/Button/index.js
@@ -1,17 +1,22 @@
 import React from "react";
+import "./style.css";
 
 class Button extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
-  handleClick = e => {
+  handleClick = (e) => {
     e.preventDefault();
     this.props.behavior();
     this.props.update();
   };
   render() {
-    return <button onClick={this.handleClick}>{this.props.value}</button>;
+    return (
+      <button onClick={this.handleClick} className="button">
+        {this.props.value}
+      </button>
+    );
   }
 }
 
