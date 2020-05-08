b5a147f2c4281a32354b95a15c46264cef0ed7ce
unknown <ashishuiux@gmail.com>
2020-05-08 14:45:57 -0700

    useState hook

--- src/components/PascalsTriangle/Row/NumberBox/index.js
+++ src/components/PascalsTriangle/Row/NumberBox/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 let NumberBox = (props) => {
+  const [boxState, updateSelected] = useState(false);
+
   return (
-    <div className="number-box">
+    <div
+      className={`number-box ${boxState ? "selected" : "not-selected"}`}
+      onClick={() => updateSelected(!boxState)}
+    >
       <span className="number">{props.number}</span>
     </div>
   );
