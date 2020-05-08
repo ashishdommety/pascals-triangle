cc4dcb340869cf7a6c73aa881813a73f99f58d74
unknown <ashishuiux@gmail.com>
2020-05-08 15:27:02 -0700

    add useState and useEffect in Form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 
 function Form(props) {
+  const [rowCount, updateRowCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`useEffect was triggered`);
+  });
+
   return (
     <div>
+      <p
+        style={{ color: "white" }}
+        id="row-count"
+        onClick={() => updateRowCount(rowCount + 1)}
+      >
+        You clicked me {rowCount} times.
+      </p>
       <Button value="+" behavior={props.increaseRow} update={props.updateRow} />
       <Button value="-" behavior={props.decreaseRow} update={props.updateRow} />
     </div>
