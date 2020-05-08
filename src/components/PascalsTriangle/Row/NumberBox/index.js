18ce3ea1c7c1357f830ba6b593c9044c12b44e94
unknown <ashishuiux@gmail.com>
2020-05-08 12:35:53 -0700

    style number in number-box

--- src/components/PascalsTriangle/Row/NumberBox/index.js
+++ src/components/PascalsTriangle/Row/NumberBox/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./style.css";
 
 let NumberBox = (props) => {
-  return <div className="number-box">{props.number}</div>;
+  return (
+    <div className="number-box">
+      <span className="number">{props.number}</span>
+    </div>
+  );
 };
 
 export default NumberBox;
